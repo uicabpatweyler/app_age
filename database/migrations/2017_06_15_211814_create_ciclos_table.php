7d4fe819f1daeedbf8d42d04906a6a80466450a0
uicabpatweyler <uicabpatweyler@gmail.com>
2017-06-15 16:46:25 -0500

    Archivo de migracion para los ciclos escolares
    Archivo de migracion para los ciclos escolares

---
+++ database/migrations/2017_06_15_211814_create_ciclos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCiclosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ciclos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->smallInteger('ciclo_anioinicial');
+            $table->smallInteger('ciclo_aniofinal');
+            $table->boolean('ciclo_actual');
+            $table->boolean('ciclo_activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ciclos');
+    }
+}
