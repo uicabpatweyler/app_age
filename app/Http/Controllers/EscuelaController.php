504130f0129eab04bffbd25bb5bcca89d35e2e8e
uicabpatweyler <uicabpatweyler@gmail.com>
2017-06-20 17:55:28 -0500

    Funciones Ajax Combos Dinamicos
    Funciones Ajax Combos Dinamicos listaAjaxNiveles y listaAjaxServicios.

--- app/Http/Controllers/EscuelaController.php
+++ app/Http/Controllers/EscuelaController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TipoDeServicio;
+use App\Models\Nivel;
+use App\Models\Servicio;
 use Illuminate\Http\Request;
 
 class EscuelaController extends Controller
 {
+    public function listaAjaxNiveles($id){
+        $niveles = Nivel::where('tiposervicio_id', $id)
+            ->select('id as value', 'nivel_nombre as text')
+            ->orderBy('id', 'ASC')
+            ->get()
+            ->toArray();
+        array_unshift($niveles, ['value' => '-1', 'text' => '[Seleccione un nivel]']);
+        return $niveles;
+    }
+
+    public function listaAjaxServicios($id){
+        $servicios = Servicio::where('nivel_id', $id)
+            ->select('id as value', 'servicio_nombre as text')
+            ->orderBy('id', 'ASC')
+            ->get()
+            ->toArray();
+        array_unshift( $servicios, ['value' => '-1', 'text' => '[Tipo de Servicio]']);
+        return  $servicios;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +36,12 @@ class EscuelaController extends Controller
      */
     public function index()
     {
-        //
+        $tiposdeservicio = TipoDeServicio::select('id', 'tiposervicio_nombre')
+                           ->where('tiposervicio_estado', 1)
+                           ->OrderBy('id','ASC')
+                           ->get();
+
+       return view('escuelas.create',compact('tiposdeservicio'));
     }
 
     /**
