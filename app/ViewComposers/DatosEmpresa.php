c51de95b58ca2a57f76d0a8fba3ea4335d657a14
uicabpatweyler <uicabpatweyler@gmail.com>
2017-06-21 17:22:33 -0500

    Actualizacion del ViewComposer
    Se agregan datos para las vistas

--- app/ViewComposers/DatosEmpresa.php
+++ app/ViewComposers/DatosEmpresa.php
@@ -15,6 +15,16 @@ class DatosEmpresaComposer{
         else{
             $empresa = Empresa::first();
             $view->with('razon_social', $empresa->empresa_razonsocial);
+            $view->with('empresa_direccion', $empresa->empresa_direccion);
+            $view->with('empresa_numexterior',$empresa->empresa_numexterior);
+            $view->with('empresa_numinterior', $empresa->empresa_numinterior);
+            $view->with('empresa_referencia', $empresa->empresa_referencia);
+            $view->with('empresa_colonia', $empresa->empresa_colonia);
+            $view->with('empresa_codigopostal',$empresa->empresa_codigopostal);
+            $view->with('empresa_delegacion', $empresa->empresa_delegacion);
+            $view->with('empresa_localidad', $empresa->empresa_localidad);
+            $view->with('empresa_estado', $empresa->empresa_estado);
+            $view->with('empresa_pais', $empresa->empresa_pais);
         }
 
     }
