19961f4d97b07de5c36d7eedb69ac70e18484df6
uicabpatweyler <uicabpatweyler@gmail.com>
2017-06-27 17:05:46 -0500

    Actualizacion de campos
    Actualizacion y correccion de nombres de campo

--- app/Models/Escuela.php
+++ app/Models/Escuela.php
@@ -13,4 +13,34 @@ class Escuela extends Model
      */
     protected $table = 'escuelas';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'escuela_tiposervicio',
+        'escuela_nivel',
+        'escuela_servicio',
+        'escuela_ciclo',
+        'escuela_nombre',
+        'escuela_clavect',
+        'escuela_numincorporacion',
+        'escuela_direccion',
+        'escuela_numexterior',
+        'escuela_numinterior',
+        'escuela_referencia',
+        'escuela_colonia',
+        'escuela_codpost',
+        'escuela_telefono',
+        'escuela_email',
+        'escuela_delegacion',
+        'escuela_localidad',
+        'escuela_estado',
+        'escuela_pais',
+        'escuela_status',
+    ];
+
+
+
 }
