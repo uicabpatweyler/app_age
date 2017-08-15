33863b30a941a214c9644116959c1dacf6ff7557
uicabpatweyler <uicabpatweyler@gmail.com>
2017-08-15 09:17:41 -0500

    Enlace hacia la vista Grupos
    Enlace hacia la vista Grupos

--- resources/views/templates/main-header.blade.php
+++ resources/views/templates/main-header.blade.php
@@ -24,7 +24,7 @@
                                     <li class="divider"></li>
                                     <li><a href="{{route('clasificaciones')}}"><i class="fa fa-caret-right text-green" aria-hidden="true"></i> Clasificación</a></li>
                                     <li class="divider"></li>
-                                    <li><a href="#"><i class="fa fa-caret-right text-green" aria-hidden="true"></i> Grupos</a></li>
+                                    <li><a href="{{route('grupos')}}"><i class="fa fa-caret-right text-green" aria-hidden="true"></i> Grupos</a></li>
                                     <li class="divider"></li>
                                 </ul>
                             </li>
